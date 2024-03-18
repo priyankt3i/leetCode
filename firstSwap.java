6c4c013f43a22a66ef547507af04a380ee16635a
Kumar Priyank <kpriyank88@gmail.com>
2024-03-18 17:33:55 -0400

    Update firstSwap.java

--- firstSwap.java
+++ firstSwap.java
@@ -18,20 +18,21 @@ public String[] firstSwap(String[] strings) {
     for (int i = 0; i < strings.length; i++) {
         String firstChar = strings[i].substring(0, 1);
         //System.out.println(firstChar);
+        
         Integer prevIndex = map.get(firstChar); // check if map has first char
 
-        if (prevIndex == null) { //if map doesnt have first char
-            map.put(firstChar, i); // add to map
-            continue;
-        }
-        if (prevIndex == swapped) { // check map if this has swapped before
-            continue;
-        }
-        
+            if (prevIndex == null) { //if map doesnt have first char
+                map.put(firstChar, i); // add to map
+                continue; //continue iteration
+            }
+            if (prevIndex == swapped) { // check map if this has swapped before
+                continue; //continue iteration
+            }
+        //swap string in array
         String temp = strings[i];
         strings[i] = strings[prevIndex];
         strings[prevIndex] = temp; 
-        
+
         map.put(firstChar, swapped); // add to map and mention swappped
     }
   
