8fd21499dce726ec03c99dd61843fe073441609f
Kumar Priyank <kpriyank88@gmail.com>
2024-03-18 17:37:11 -0400

    Update findDuplicates.java

--- findDuplicates.java
+++ findDuplicates.java
@@ -5,23 +5,24 @@ find duplicates and count of duplicates of each element in a string array.
 */
 
 class Solution {
-    public map<String, Integer> findDuplicates(string[] strings) {
+    public Map<String, Integer> findDuplicates(string[] strings) {
 
-        String[] arr = {"kumar", "kumar", "priyank", "kumar", "xyz", "abc", "priyank"};
-        HashMap<String, Integer> map = new HashMap<>();
+        //String[] arr = {"kumar", "kumar", "priyank", "kumar", "xyz", "abc", "priyank"};
+        Map<String, Integer> map = new HashMap<>();
         
-        for(int i =0;i<arr.length;i++){
+        for(int i =0;i<strings.length;i++){
   	        int count=0;
-  	        for(int j = 0; j < arr.length; j++){      
-    			   if(i!=j && arr[i].equals(arr[j])){
+  	        for(int j = 0; j < strings.length; j++){      
+    			   if(i!=j && strings[i].equals(strings[j])){
     			    count=count+1;
     			   }
-  				 if(count>0){
-  				   map.put(arr[i],count);
-  				 }
+				if(count>0){
+				   map.put(strings[i],count);
+				 }
 	        }
         }
     
-        map.forEach((key, value) -> System.out.println(key + " " + value));
+        //map.forEach((key, value) -> System.out.println(key + " " + value));
+	return map;
     }
 }
